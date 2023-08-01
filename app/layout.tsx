03911d666acbfc0638585abae4618a48f5d3e534
angelxtry <angelxtry@gmail.com>
2023-08-01 23:58:11 +0900

    ts setting

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import {ReactNode} from "react";
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
+const RootLayout = ({children}: RootLayoutProps) => {
+  return (
+    <html lang="en">
+      <body>
+        <header>
+          [header]
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer>
+          [footer]
+        </footer>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
