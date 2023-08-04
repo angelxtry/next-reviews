f1b5fb2b1de6917a2a9932e6cefaa62bab81f38e
angelxtry <angelxtry@gmail.com>
2023-08-04 22:46:47 +0900

    ✨ 단순한 nav, footer 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react';
 
+import Link from 'next/link';
+
 interface RootLayoutProps {
   children: ReactNode;
 }
@@ -7,9 +9,32 @@ const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
       <body>
-        <header>[header]</header>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/reviews">Reviews</Link>
+              </li>
+              <li>
+                <Link href="/about" prefetch={false}>
+                  About
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
         <main>{children}</main>
-        <footer>[footer]</footer>
+
+        <footer>
+          Game data and images courtesy of{' '}
+          <a href="https://rawg.io/" target="_blank">
+            RAWG
+          </a>
+        </footer>
       </body>
     </html>
   );
