a90c1f5f8fcc8c29d78852c8ded26ce063b8a05f
angelxtry <angelxtry@gmail.com>
2023-08-05 00:02:39 +0900

    ✨ layout styling, NavBar 컴포넌트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 
 import { ReactNode } from 'react';
 
-import Link from 'next/link';
+import { NavBar } from '../components/NavBar';
 
 interface RootLayoutProps {
   children: ReactNode;
@@ -10,30 +10,16 @@ interface RootLayoutProps {
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
-      <body>
+      <body className="flex min-h-screen flex-col bg-orange-50 px-4 py-2">
         <header>
-          <nav>
-            <ul>
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/reviews">Reviews</Link>
-              </li>
-              <li>
-                <Link href="/about" prefetch={false}>
-                  About
-                </Link>
-              </li>
-            </ul>
-          </nav>
+          <NavBar />
         </header>
 
-        <main>{children}</main>
+        <main className="grow py-3">{children}</main>
 
-        <footer>
+        <footer className="border-t py-3 text-center text-xs">
           Game data and images courtesy of{' '}
-          <a href="https://rawg.io/" target="_blank">
+          <a href="https://rawg.io/" target="_blank" className="text-orange-800 hover:underline">
             RAWG
           </a>
         </footer>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{tsx}',
-    './components/**/*.{tsx}',
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {},
