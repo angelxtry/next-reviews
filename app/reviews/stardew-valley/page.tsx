2e6483e37908c5d7854fd9320c3b75cb97bef2c4
angelxtry <angelxtry@gmail.com>
2023-08-04 23:30:57 +0900

    ✨ reviews nav 추가

---
+++ app/reviews/stardew-valley/page.tsx
@@ -0,0 +1,5 @@
+const StardewValleyPage = () => {
+  return <h1>Stardew Valley Reviews</h1>;
+};
+
+export default StardewValleyPage;
--- app/reviews/stardew-vally/page.tsx
+++
@@ -1,5 +0,0 @@
-const StardewVallyPage = () => {
-  return <h1>Stardew Vally Reviews</h1>;
-};
-
-export default StardewVallyPage;
