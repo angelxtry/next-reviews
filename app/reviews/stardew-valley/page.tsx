9e7563c1e57feb8876a2995ac65e00edd38541bb
angelxtry <angelxtry@gmail.com>
2023-08-05 21:04:21 +0900

    ✨ front matter 를 이용하여 데이터 출력 수정

--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -1,23 +1,23 @@
 import { readFile } from 'node:fs/promises';
 
+import matter from 'gray-matter';
 import { marked } from 'marked';
 
 import { Heading } from '@/components/heading';
 
 const StardewValleyPage = async () => {
   const text = await readFile('./contents/reviews/stardew-valley.md', 'utf-8');
-  const html = marked(text);
+  const {
+    content,
+    data: { title, date, image },
+  } = matter(text);
+  const html = marked(content);
 
   return (
     <div>
-      <Heading>Stardew Valley</Heading>
-      <img
-        src="/images/stardew-valley.jpg"
-        alt="stardew valley"
-        width="640"
-        height="360"
-        className="mb-2 rounded"
-      />
+      <Heading>{title}</Heading>
+      <p className="pb-2 italic">{date}</p>
+      <img src={image} alt={title} width="640" height="360" className="mb-2 rounded" />
       <article
         dangerouslySetInnerHTML={{ __html: html }}
         className="prose prose-slate max-w-screen-sm"
