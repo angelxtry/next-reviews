f61e711e1d9b21f58c43cb910dc01cc5d4680751
angelxtry <angelxtry@gmail.com>
2023-08-05 13:47:14 +0900

    ✨ responsive 스타일 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+import Link from 'next/link';
+
 import { Heading } from '@/components/heading';
 
 const HomePage = () => {
   return (
     <>
       <Heading>Indie Gamer</Heading>
-      <p>Only the best indie games, reviewed for you.</p>
+      <p className="pb-3">Only the best indie games, reviewed for you.</p>
+      <div className="w-80 rounded border bg-white font-orbitron font-semibold shadow hover:shadow-xl sm:w-full">
+        <Link href="/reviews/stardew-valley" className="flex flex-col sm:flex-row">
+          <img
+            src="/images/stardew-valley.jpg"
+            alt="stardew valley"
+            width="320"
+            height="180"
+            className="rounded-t sm:rounded-l sm:rounded-r-none"
+          />
+          <h2 className="py-1 text-center sm:px-2">Stardew Valley</h2>
+        </Link>
+      </div>
     </>
   );
 };
