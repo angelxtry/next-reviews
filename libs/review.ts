25ad87761d9ce1b1a688351dffb4b0d124b3f0fe
angelxtry <angelxtry@gmail.com>
2023-08-07 01:10:53 +0900

    ✨ refactoring review.ts

--- libs/review.ts
+++ libs/review.ts
@@ -22,54 +22,61 @@ interface Review {
   };
 }
 
+interface Params {
+  filters?: { [key: string]: { $eq: string } };
+  fields?: string[];
+  populate?: { [key: string]: { fields: string[] } };
+  sort?: string[];
+  pagination?: { pageSize: number; withCount?: boolean };
+}
+
 const CmsUrl = 'http://127.0.0.1:1337' as const;
 
-const getReview = async (slug: string) => {
-  const url =
-    `${CmsUrl}/api/reviews?` +
-    qs.stringify(
-      {
-        filters: { slug: { $eq: slug } },
-        fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
-        populate: { image: { fields: ['url'] } },
-        sort: ['publishedAt:desc'],
-        pagination: { pageSize: 1, withCount: false },
-      },
-      { encodeValuesOnly: true },
-    );
+const fetchReviews = async (params: Params) => {
+  const url = `${CmsUrl}/api/reviews?${qs.stringify(params, { encodeValuesOnly: true })}`;
   const response = await fetch(url);
-  const { data } = (await response.json()) as { data: Review[] };
+  if (!response.ok) {
+    throw new Error(`CMS returned ${response.status} for ${url}`);
+  }
+  return (await response.json()) as { data: Review[] };
+};
+
+const toReview = (item: Review) => {
+  const {
+    attributes: { slug, title, subtitle, publishedAt, image },
+  } = item;
+  return {
+    slug,
+    title,
+    subtitle,
+    date: publishedAt.slice(0, 'yyyy-mm-dd'.length),
+    image: CmsUrl + image.data[0].attributes.url,
+  };
+};
+
+const getReview = async (slug: string) => {
+  const { data } = await fetchReviews({
+    filters: { slug: { $eq: slug } },
+    fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
+    populate: { image: { fields: ['url'] } },
+    sort: ['publishedAt:desc'],
+    pagination: { pageSize: 1, withCount: false },
+  });
   const item = data[0];
   return {
-    slug: item.attributes.slug,
-    title: item.attributes.title,
-    subtitle: item.attributes.subtitle,
-    date: item.attributes.publishedAt.slice(0, 'yyyy-mm-dd'.length),
-    image: CmsUrl + item.attributes.image.data[0].attributes.url,
+    ...toReview(item),
     body: marked(item.attributes.body, { headerIds: false, mangle: false }),
   };
 };
 
 const getReviews = async () => {
-  const url =
-    `${CmsUrl}/api/reviews?` +
-    qs.stringify(
-      {
-        fields: ['slug', 'title', 'publishedAt'],
-        populate: { image: { fields: ['url'] } },
-        sort: ['publishedAt:desc'],
-        pagination: { pageSize: 6 },
-      },
-      { encodeValuesOnly: true },
-    );
-  const response = await fetch(url);
-  const { data } = (await response.json()) as { data: Review[] };
-  return data.map(({ attributes: { slug, title, publishedAt, image } }) => ({
-    slug,
-    title,
-    date: publishedAt.slice(0, 'yyyy-mm-dd'.length),
-    image: CmsUrl + image.data[0].attributes.url,
-  }));
+  const { data } = await fetchReviews({
+    fields: ['slug', 'title', 'publishedAt'],
+    populate: { image: { fields: ['url'] } },
+    sort: ['publishedAt:desc'],
+    pagination: { pageSize: 6 },
+  });
+  return data.map((item) => toReview(item));
 };
 
 const getFeaturedReviews = async () => {
